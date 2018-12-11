bef47a3214417e068a4a71906928eeec9839c333
wjj <wjj99@qq.com>
2018-12-11 16:04:07 +0800

    折线图添加渐变色阴影。

--- app/src/main/java/com/example/whl/myapplication/MainActivity.java
+++ app/src/main/java/com/example/whl/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.whl.myapplication;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +19,7 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
                     isWeek = true;
                     setWeekData();
 
-
                     lineChartWeek.notifyDataSetChanged(); // let the chart know it's data changed
                     lineChartWeek.invalidate(); // refresh
 
@@ -92,93 +94,93 @@ public class MainActivity extends AppCompatActivity {
                 entriesWeek.add(new Entry(i, 0));
             }
         }
-        //一个LineDataSet就是一条线
-        lineDataSet = new LineDataSet(entriesWeek, "时长");
-        //一个LineDataSet就是一条线
-        // LineDataSet lineDataSet = new LineDataSet(entriesMonth, "时长");
-        // lineDataSet.clear();
-        //线颜色
-        lineDataSet.setColor(getResources().getColor(R.color.gray_e6));
-        //线宽度
-        lineDataSet.setLineWidth(2f);
 
-        //设置折线图填充
-        //lineDataSet.setDrawFilled(true);
-
-        //设置曲线值的圆点是实心还是空心
-        lineDataSet.setDrawCircleHole(true);
-        lineDataSet.setCircleRadius(3f);
-        lineDataSet.setCircleColor(getResources().getColor(R.color.yellow_bg));
-
-        lineDataSet.setValueTextColor(getResources().getColor(R.color.font3));//折点文字
-        // lineDataSet.set
-        // lineDataSet.setDrawValues(false);
-        //设置显示值的字体大小
-        lineDataSet.setValueTextSize(12f);
-
-        lineDataSet.setDrawValues(false);////不显示数值
-
-        //lineDataSet.clic
-        //线模式为圆滑曲线（默认折线）
-        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-
-
-        LineData data = new LineData(lineDataSet);
-        lineChartWeek.setData(data);
-
-
-        lineChartWeek.getLegend().setEnabled(false);
-
-//        lineChartWeek.setHighlightPerDragEnabled(false);
-//        lineChartWeek.setHighlightPerTapEnabled(false);
-
-
-        Description mDescription = new Description();
-        mDescription.setText("");
-        lineChartWeek.setDescription(mDescription);
-
-
-        lineChartWeek.setMarker(new ChartMarkerView(this, R.layout.layout, "f:", "数值："));
-
-
-        //是否缩放Y轴
-        lineChartWeek.setScaleYEnabled(false);
-        //设置是否可以通过双击屏幕放大图表。默认是true
-        //lineChartWeek.setDoubleTapToZoomEnabled(false);
-        XAxis xAxis = lineChartWeek.getXAxis();//得到X轴：
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//设置X轴的位置（默认在上方）：
-        xAxis.setGranularity(1f);//设置X轴坐标之间的最小间隔（因为此图有缩放功能，X轴,Y轴可设置可缩放）
-        //xAxis.setLabelCount(12, true);
-        xAxis.setDrawAxisLine(false);
-        xAxis.setGridColor(getResources().getColor(R.color.transparent));
-        xAxis.setTextColor(getResources().getColor(R.color.font2));
-
-        xAxis.setTextSize(12f);
-//        //设置X轴值为字符串
-        xAxis.setValueFormatter(new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-
-                String ss = "";
-                try {
-                    ss = mListWeek.get((int) value);
-                } catch (IndexOutOfBoundsException e) {
-                    e.printStackTrace();
-                    ss = "ha";
-                }
-
-
-                return ss; //mList为存有月份的
-            }
-        });
-
-
-        //  mLineChart.ge
-
-        YAxis leftYAxis = lineChartWeek.getAxisLeft();
-        YAxis rightYAxis = lineChartWeek.getAxisRight();
-        rightYAxis.setEnabled(false); //右侧Y轴不显示
-        leftYAxis.setEnabled(false);
+        initChartView();
+//        //一个LineDataSet就是一条线
+//        lineDataSet = new LineDataSet(entriesWeek, "时长");
+//        //一个LineDataSet就是一条线
+//        // LineDataSet lineDataSet = new LineDataSet(entriesMonth, "时长");
+//        // lineDataSet.clear();
+//        //线颜色
+//        lineDataSet.setColor(getResources().getColor(R.color.gray_e6));
+//        //线宽度
+//        lineDataSet.setLineWidth(2f);
+//
+//        //设置折线图填充
+//        //lineDataSet.setDrawFilled(true);
+//
+//        //设置曲线值的圆点是实心还是空心
+//        lineDataSet.setDrawCircleHole(true);
+//        lineDataSet.setCircleRadius(3f);
+//        lineDataSet.setCircleColor(getResources().getColor(R.color.yellow_bg));
+//
+//        lineDataSet.setValueTextColor(getResources().getColor(R.color.font3));//折点文字
+//        // lineDataSet.set
+//        // lineDataSet.setDrawValues(false);
+//        //设置显示值的字体大小
+//        lineDataSet.setValueTextSize(12f);
+//
+//        lineDataSet.setDrawValues(false);////不显示数值
+//
+//        //lineDataSet.clic
+//        //线模式为圆滑曲线（默认折线）
+//        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+//
+//
+//        LineData data = new LineData(lineDataSet);
+//        lineChartWeek.setData(data);
+//
+//
+//        lineChartWeek.getLegend().setEnabled(false);
+//
+////        lineChartWeek.setHighlightPerDragEnabled(false);
+////        lineChartWeek.setHighlightPerTapEnabled(false);
+//
+//
+//        Description mDescription = new Description();
+//        mDescription.setText("");
+//        lineChartWeek.setDescription(mDescription);
+//
+//
+//        lineChartWeek.setMarker(new ChartMarkerView(this, R.layout.layout, "f:", "数值："));
+//
+//
+//        //是否缩放Y轴
+//        lineChartWeek.setScaleYEnabled(false);
+//        //设置是否可以通过双击屏幕放大图表。默认是true
+//        //lineChartWeek.setDoubleTapToZoomEnabled(false);
+//        XAxis xAxis = lineChartWeek.getXAxis();//得到X轴：
+//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//设置X轴的位置（默认在上方）：
+//        xAxis.setGranularity(1f);//设置X轴坐标之间的最小间隔（因为此图有缩放功能，X轴,Y轴可设置可缩放）
+//        //xAxis.setLabelCount(12, true);
+//        xAxis.setDrawAxisLine(false);
+//        xAxis.setGridColor(getResources().getColor(R.color.transparent));
+//        xAxis.setTextColor(getResources().getColor(R.color.font2));
+//
+//        xAxis.setTextSize(12f);
+////        //设置X轴值为字符串
+//        xAxis.setValueFormatter(new IAxisValueFormatter() {
+//            @Override
+//            public String getFormattedValue(float value, AxisBase axis) {
+//
+//                String ss = "";
+//                try {
+//                    ss = mListWeek.get((int) value);
+//                } catch (IndexOutOfBoundsException e) {
+//                    e.printStackTrace();
+//                    ss = "ha";
+//                }
+//                return ss; //mList为存有月份的
+//            }
+//        });
+//
+//
+//        //  mLineChart.ge
+//
+//        YAxis leftYAxis = lineChartWeek.getAxisLeft();
+//        YAxis rightYAxis = lineChartWeek.getAxisRight();
+//        rightYAxis.setEnabled(false); //右侧Y轴不显示
+//        leftYAxis.setEnabled(false);
     }
 
     /**
@@ -197,6 +199,11 @@ public class MainActivity extends AppCompatActivity {
                 entriesWeek.add(new Entry(i, 0));
             }
         }
+        initChartView();
+
+    }
+
+    private void initChartView() {
         //一个LineDataSet就是一条线
         lineDataSet = new LineDataSet(entriesWeek, "时长");
         // lineDataSet.clear();
@@ -205,11 +212,22 @@ public class MainActivity extends AppCompatActivity {
         //线宽度
         lineDataSet.setLineWidth(2f);
 
+        if (Utils.getSDKInt() >= 18) {
+            // fill drawable only supported on api level 18 and above
+            Drawable drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.bg_zhexian);
+            lineDataSet.setFillDrawable(drawable);
+        }
+        else {
+            lineDataSet.setFillColor(Color.YELLOW);
+        }
+        //设置折线图填充
+        lineDataSet.setDrawFilled(true);
+
         //设置折线图填充
         //lineDataSet.setDrawFilled(true);
 
         //设置曲线值的圆点是实心还是空心
-        lineDataSet.setDrawCircleHole(true);
+        lineDataSet.setDrawCircleHole(true);//true空心,false实心;
         lineDataSet.setCircleRadius(3f);
         //lineDataSet.setc
         lineDataSet.setCircleColor(getResources().getColor(R.color.yellow_bg));
